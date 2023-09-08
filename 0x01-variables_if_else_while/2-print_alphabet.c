24ac471aa21073a4b8e2034180aff50fe7f799bd
Precious <eseprecious934@gmail.com>
2023-09-08 14:07:37 -0700

    insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase alphabets
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	int l;
+	l = 'a';
+
+	while (l <= 'z')
+	{
+		putchar(l);
+	l++;
+	}
+	putchar('\n');
+	return (0);
+}
