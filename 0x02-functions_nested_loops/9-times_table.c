228843602192da0ade1cfb5c0b88cd86de010f71
Precious <eseprecious934@gmail.com>
2023-09-13 15:58:44 -0700

    add

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,25 +18,22 @@ void times_table(void)
 		{
 			int c = a * b;
 
-			if ( c >= 10)
+			if (c >= 10)
 			{
+				_putchar(',');
+				_putchar(' ');
 			_putchar((c / 10) + '0');
 			_putchar((c % 10) + '0');
-			if (b < 9)
-			{
-			_putchar(',');
-			_putchar(' ');
 			}
-			}
-			else if (c < 10)
+			else
 			{
-				_putchar((c % 10) + '0');
-				if (b < 9)
+				if (b != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 				}
+				_putchar(c + '0');
 			}
 			b++;
 		}
