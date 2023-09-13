b1f18d7405e1f86d62cd0fa2f89223255594c1e5
Precious <eseprecious934@gmail.com>
2023-09-12 21:15:19 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - program to print _putchar
+ *
+ * Return: 0 on swuccess
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
