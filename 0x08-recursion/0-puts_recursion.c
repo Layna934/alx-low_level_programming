a6af6ea7d3b05fac7f943c3b20b5adb3ebe5e483
Precious <eseprecious934@gmail.com>
2023-09-28 21:58:03 -0700

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,14 +3,14 @@
 
 /**
   *_puts_recursion - prints a string using recursion
-  *@s - pointer to astring
+  *@s: pointer to astring
   *
   *return: void
   */
 
 void _puts_recursion(char *s)
 {
-	if(*s == 0)
+	if (*s == 0)
 	{
 		_putchar('\n');
 		return;
