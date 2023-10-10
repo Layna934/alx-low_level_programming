ce8350a8766830532f20fe678e547220f83d001d
Precious <eseprecious934@gmail.com>
2023-10-10 13:52:41 -0700

    fixed betty

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,8 +14,6 @@
   *Return: nothing
   */
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
