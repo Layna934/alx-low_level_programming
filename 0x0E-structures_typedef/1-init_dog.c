ad93953a6b22c7e64ea3c0471639dcce0e7e4fc0
Precious <eseprecious934@gmail.com>
2023-10-10 04:48:43 -0700

    rewrote the code

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -20,18 +20,17 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 		return;
+	d->name = malloc(sizeof(char) * (strlen(name) + 1));
+	if (d->name == NULL)
+		exit(98);
+	strcpy(d->name, name);
 
-	(*d).name = malloc(strlen(name) + 1);
-	if ((*d).name == NULL)
+	d->age = age;
+
+	d->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	if (d->owner == NULL)
 	{
 		exit(98);
 	}
-	strcpy((*d).name, name);
-
-	(*d).age = age;
-
-	(*d).owner = malloc(sizeof(char) * (strlen(owner) + 1));
-	if ((*d).owner == NULL)
-		exit(98);
-	strcpy((*d).owner, owner);
+	strcpy(d->owner, owner);
 }
