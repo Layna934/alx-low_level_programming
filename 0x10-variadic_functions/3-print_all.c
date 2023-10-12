d83b223df5d25a60a50553ed2fbc56645fa6627e
Precious <eseprecious934@gmail.com>
2023-10-12 15:27:56 -0700

    light

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,56 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+  *print_all - prints all
+  *@format: the format specifier
+  *
+  *Return: nothing
+  */
+
+void print_all(const char * const format, ...)
+{
+	int m = 0, length = strlen(format);
+	va_list flist;
+
+	va_start(flist, format);
+	while (format[m] != '\0')
+	{
+		switch (format[m])
+		{
+			case 'c':	{
+					char val = va_arg(flist, int);
+
+					printf("%c", val);
+				}
+				break;
+			case 'i':	{
+					int val = va_arg(flist, int);
+
+					printf("%d", val);
+				}
+				break;
+			case 'f':       {
+						float val = va_arg(flist, double);
+
+						printf("%f", val);
+					}
+                                  break;
+			case 's':	{
+						char * val = va_arg(flist, char *);
+
+						printf("%s", val);
+					}
+					break;	  
+			default:
+				break;
+		}
+		m++;
+		va_end(flist);
+		while (m < length -1)
+			if (format[m] ='c' || format[m] = 'i' || format[m] = 'f' || format[m] = 's')
+				printf(", ");
+	}
+}
