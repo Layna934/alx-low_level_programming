d59dd4a5db2566e955818559385fd59c37f35edf
Precious <eseprecious934@gmail.com>
2023-10-19 17:06:57 -0700

    added node at end

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -30,6 +30,6 @@ list_t *add_node(list_t **head, const char *str)
 	}
 	new->len = strlen(str);
 	new->next = *head;
-	*head = new_node;
+	*head = new;
 	return (new);
 }
---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+  *add_node_end - adds node at list end
+  *@head: the head
+  *@str: the tsring to be added
+  *
+  *Return: address of new element or NULL for fail
+  */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *current;
+
+	if (str == NULL)
+		return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->len = strlen(str);
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		current = *head;
+		if (current != NULL)
+		{
+			current = current->next;
+		}
+
+		current->next = new;
+	}
+	return (new);
+}
