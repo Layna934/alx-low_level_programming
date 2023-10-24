72c0d6a1abd36f09a621528ed5eb27669de17565
Precious <eseprecious934@gmail.com>
2023-10-24 13:56:29 -0700

    listed

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  *listint_len - function to return length of list
+  *@h: particular list pointer
+  *
+  *Return: the calculated length
+  */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while ( h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
