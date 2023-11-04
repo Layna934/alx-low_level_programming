d7a91b9937ab76f412b0a8539c58e447d9c7d579
Precious <eseprecious934@gmail.com>
2023-11-03 21:53:29 -0700

    fixed betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,23 +15,25 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	 int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	 ssize_t bytes_written = write(fd, text_content, strlen(text_content));
+	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	ssize_t bytes_written = write(fd, text_content, strlen(text_content));
 
-	 if (filename == NULL)
-		 return (-1);
+	if (filename == NULL)
+	{
+		return (-1);
+	}
 
-	 if (fd == -1)
-		 return (-1);
+	if (fd == -1)
+		return (-1);
 
-	 if (text_content != NULL)
-	 {
-		 if (bytes_written == -1)
-		 {
-			 close(fd);
-			 return (-1);
-		 }
-	 }
-	 close(fd);
-	 return (1);
+	if (text_content != NULL)
+	{
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
 }
