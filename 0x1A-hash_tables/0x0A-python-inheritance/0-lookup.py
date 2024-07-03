e573b1432770c5e22700ed6e0d46119c7858362c
0xPrecious <eseprecious934@gmail.com>
2024-07-02 17:09:00 -0700

    lookup

---
+++ 0x1A-hash_tables/0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+Lists attributes and methods of an object
+"""
+
+def lookup(obj):
+    """
+    Returns list of attributes and methods of object
+    Args:
+    - obj: object
+    """
+
+    return dir(obj)
